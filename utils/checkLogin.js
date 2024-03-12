625fa6890899c97c19dbb9ca95590ea95469870b
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-03-12 16:20:25 +0200

    Update checkLogin.js

--- utils/checkLogin.js
+++ utils/checkLogin.js
@@ -4,9 +4,6 @@ const isUserLoggedIn = (req, res, next) => {
     const cookies = req.headers.cookie || '';
     const tokenCookie = cookie.parse(cookies).token;
 
-
-    console.log("Header : ", req.headers.cookie);
-
     if (!tokenCookie) {
         req.isAuthenticated = false
     }else{
