1c9de889889b10090a054174c56d1508a8ccfcb6
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-03-14 17:22:10 +0200

    Update nodemailer.js

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,53 +1,52 @@
+
+
 const nodemailer = require("nodemailer");
+const emailTemplateRegistration = require("./emailTemplate");
 
-function generateStudentNumber() {
-  // Generate a random 8-digit student number starting with 22
-  const studentNumber = `22${Math.floor(10000000 + Math.random() * 90000000)}`;
-  return studentNumber;
+async function sendMessage(     
+  first_name,
+  last_name,
+  identification_number,
+  email,
+  user_role,
+  subject,) {
+  try {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: process.env.APP_USER, // email
+        pass: process.env.APP_PASSWORD, // app password
+      },
+    });
+
+    // Send user's message
+    let info = await transporter.sendMail({
+      sender: `${email}`,
+      from: `${email}`, // sender address
+      to: "goodnessjakazac@gmail.com",
+      replyTo: `${email}`,
+      subject: `${subject}`, // Subject line
+    });
+
+    // Generate and send confirmation email with student number
+
+    info = await transporter.sendMail({
+      sender: "goodnessjakazac@gmail.com",
+      from: "goodnessjakazac@gmail.com", // sender address
+      to: `${email}`,
+      replyTo: 'goodnessjakazac@gmail.com',
+      subject: `${subject}`, // Subject line
+      html: `${emailTemplateRegistration( first_name , last_name ,identification_number , user_role )}`, // html body
+    });
+
+    return true; // Return true if email was sent successfully
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return false; // Return false if an error occurred while sending email
+  }
 }
 
-async function sendMessage(userName, userEmail, userSubject, userMessage) {
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: process.env.APP_USER, // email
-      pass: process.env.APP_PASSWORD, // app password
-    },
-  });
-
-  // Send user's message
-  let info = await transporter.sendMail({
-    sender: `${userEmail}`,
-    from: `${userEmail}`, // sender address
-    to: "goodnessjakazac@gmail.com",
-    replyTo: `${userEmail}`,
-    subject: `${userSubject}`, // Subject line
-    text: `${userMessage}`, // plain text body
-    html: `<b>${userMessage}</b>`, // html body
-  });
-
-  // Generate and send confirmation email with student number
-  const studentNumber = generateStudentNumber();
-  info = await transporter.sendMail({
-    sender: "goodnessjakazac@gmail.com",
-    from: "goodnessjakazac@gmail.com", // sender address
-    to: `${userEmail}`,
-    replyTo: 'goodnessjakazac@gmail.com',
-    subject: `Registration Confirmation`, // Subject line
-    text: `Hey ${userName}, Your student number is: ${studentNumber}`, // plain text body
-    html: `
-      <p>Dear <b>${userName}</b>,</p>
-      <p>Your registration was successful!</p>
-      <p>Your student number is: <b>${studentNumber}</b></p>
-      <p>This is to confirm that we have received your registration details.</p>
-      <p>We will get back to you as soon as possible.</p>
-      <p>Regards,</p>
-      <p>Themba G Chauke</p>
-    `, // html body
-  });
-}
-
-//module.exports = { sendMessage };
+module.exports = { sendMessage };
