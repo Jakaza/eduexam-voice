dfae18cb0bfc3bd8350b93e3d59da15e539838df
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-04-09 07:04:31 +0100

    Fix bugs and Remove Some Codes

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const routes = require("./routes");
 const passport = require("passport");
-const initDB = require('./config/initDB')
-const {sendMessage} = require('./utils/nodemailer')
+const initDB = require("./config/initDB");
+const { sendMessage } = require("./utils/nodemailer");
 //  middlewares config
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
@@ -21,21 +21,36 @@ require("./config/passport");
 app.use(passport.initialize());
 // routes
 app.use(routes);
-
 app.get("/email", (req, res) => res.render("student/emaiTest"));
 app.post("/email/account", async (req, res) => {
   // Call sendMessage and get the emailStatus
-  const emailStatus = await sendMessage("Jakaza", req.body.email, "Testing if it works", "Some message");
+  const emailStatus = await sendMessage(
+    "Jakaza",
+    req.body.email,
+    "Testing if it works",
+    "Some message"
+  );
 
   if (emailStatus) {
     console.log("Email sent successfully.");
-    res.status(201).json({ status: true, message: "Test created successfully.", emailStatus: true });
+    res
+      .status(201)
+      .json({
+        status: true,
+        message: "Test created successfully.",
+        emailStatus: true,
+      });
   } else {
     console.log("Failed to send email.");
-    res.status(500).json({ status: false, message: "Failed to send email.", emailStatus: false });
+    res
+      .status(500)
+      .json({
+        status: false,
+        message: "Failed to send email.",
+        emailStatus: false,
+      });
   }
 });
-
 
 app.get("/*", (req, res) => res.render("error/404"));
 const server = http.createServer(app);
